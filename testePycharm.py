b4b36f342014408b32912429b2596f29ed0a87e9
RICARDO LINO <ricardo@costalino.com>
2020-05-18 12:29:59 -0300

    Adjusted docString

--- testePycharm.py
+++ testePycharm.py
@@ -1,12 +1,13 @@
 ## Teste Pycharm
 
 class Person:
+    ''' Classe Person'''
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def __str__(self):
-        ''' Retona Nome e idade da classe str'''
+        ''' Retona Nome e idade da classe str '''
         return f"Pessoa de nome {self.name} com {str(self.age)} anos de idade."
 
 if __name__ == "__main__":
